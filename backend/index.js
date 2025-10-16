fbca982db70dc9c89a75563797da81303d31615c
Prabhath Nugekotuwa <prabhathnugekotuwa@gmail.com>
2025-10-16 23:35:51 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://nko-frontend.vercel.app",
     credentials: true,
 }));
 
